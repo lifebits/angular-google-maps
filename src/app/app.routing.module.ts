bcfe7a5cbb07e081651aaf971f9b5d9f84215e1b
Nikolay Popov <mail@lifebits.ru>
2017-03-08 21:59:19 +0700

    Added Preloading Strategy

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,10 +1,7 @@
 import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import {RouterModule, PreloadAllModules, Routes} from '@angular/router';
 
 import {MapsComponent} from './maps/maps.component';
-import {HelpComponent} from './pages/help/help.component';
-import {AboutComponent} from './pages/about/about.component';
-
 
 const routes: Routes = [
 
@@ -15,21 +12,16 @@ const routes: Routes = [
       path: 'pages',
       loadChildren: './pages/pages.module#PagesModule'
    },
-   /*{
-      path: 'pages',
-      children: [
-         //{path: '', redirectTo: 'about', pathMatch: 'full'},
-         {path: 'help', component: HelpComponent},
-         {path: 'about', component: AboutComponent}
-      ]
-   }*/
-
-   //{ path: 'lazy', loadChildren: './lazy/lazy.module#LazyModule' }
 
 ];
 
 @NgModule ({
-   imports: [ RouterModule.forRoot(routes) ],
+   imports: [
+      RouterModule.forRoot(
+         routes,
+         { preloadingStrategy: PreloadAllModules }
+      )
+   ],
    exports: [ RouterModule ]
 })
 export class AppRoutingModule {}
